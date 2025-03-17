29073c0907b2dcb82ce57db145e18d4f7dd02f5e
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-03-17 15:45:03 +0200

    Create auth.module.ts

---
+++ music-backend/src/auth/auth.module.ts
@@ -0,0 +1,31 @@
+import { Global, Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { ArtistModule } from 'src/artist/artist.module';
+
+@Global()
+@Module({
+  imports: [
+    UserModule,
+    ArtistModule,
+    JwtModule.registerAsync({
+      useFactory: (config: ConfigService) => {
+        return {
+          global: true,
+          secret: config.get<string>('JWT_SECRET_KEY'),
+          signOptions: {
+            expiresIn: config.get<string | number>('JWT_EXPIRATION_TIME'),
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService, JwtModule],
+})
+export class AuthModule {}
