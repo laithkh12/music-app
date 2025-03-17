8bb8ed0a33d1d1e41cffbd27e9b2abd5d5d30e6a
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-03-17 15:50:07 +0200

    Create update-album.dto.ts

---
+++ music-backend/src/album/dto/update-album.dto.ts
@@ -0,0 +1,10 @@
+import { IsDateString, IsOptional, IsString } from 'class-validator';
+
+export class UpdateAlbumDto {
+  @IsOptional()
+  @IsString()
+  title: string;
+  @IsOptional()
+  @IsDateString()
+  releaseDate?: Date;
+}
