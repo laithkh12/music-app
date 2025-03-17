639e2767fe9b4b53bc4d3a759a7ea075bc0fd487
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-03-17 15:20:26 +0200

    Create app.module.ts

---
+++ music-backend/src/app.module.ts
@@ -0,0 +1,48 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { SongModule } from './song/song.module';
+import { ArtistModule } from './artist/artist.module';
+import { AlbumModule } from './album/album.module';
+import { PlaylistModule } from './playlist/playlist.module';
+import { GenreModule } from './genre/genre.module';
+import { CommentModule } from './comment/comment.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { DelayMiddleware } from './_core/middleware/delay.middleware';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+    }),
+
+    UserModule,
+    AuthModule,
+    SongModule,
+    ArtistModule,
+    AlbumModule,
+    PlaylistModule,
+    GenreModule,
+    CommentModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(DelayMiddleware).forRoutes('*');
+  }
+}
