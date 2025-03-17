e8946121a87ea182ea41af53de2316a1df3f81ac
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-03-17 15:44:48 +0200

    Create auth.controller.ts

---
+++ music-backend/src/auth/auth.controller.ts
@@ -0,0 +1,44 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { TransformDTO } from 'src/_core/interceptors/transform-dto.interceptor';
+import { AuthService } from './auth.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { ResponseAuthDto } from './dto/response-auth.dto';
+import { SignInAuthDto } from './dto/sign-in-auth.dto';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { CreateAuthSocial } from './dto/create-auth-social';
+
+@Controller('auth')
+@TransformDTO(ResponseAuthDto)
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/sign-up')
+  @HttpCode(HttpStatus.OK)
+  create(@Body() createAuthDto: CreateAuthDto) {
+    return this.authService.create(createAuthDto);
+  }
+
+  @Post('/sign-in')
+  @HttpCode(HttpStatus.OK)
+  signIn(@Body() signInAuth: SignInAuthDto) {
+    return this.authService.signIn(signInAuth);
+  }
+
+  @Post('/sign-in-social')
+  @HttpCode(HttpStatus.OK)
+  signInSocial(@Body() signInAuth: CreateAuthSocial) {
+    return this.authService.signInSocial(signInAuth);
+  }
+
+  @Post('/sign-up-artist')
+  @HttpCode(HttpStatus.OK)
+  signUpArtist(@Body() createAuthDto: CreateArtistDto) {
+    return this.authService.signUpArtist(createAuthDto);
+  }
+
+  @Post('/sign-in-artist')
+  @HttpCode(HttpStatus.OK)
+  signInArtist(@Body() signInAuth: SignInAuthDto) {
+    return this.authService.signInArtist(signInAuth);
+  }
+}
