265e868b20507741139a96048baf99c171319b1f
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-03-17 15:45:19 +0200

    Create auth.service.ts

---
+++ music-backend/src/auth/auth.service.ts
@@ -0,0 +1,95 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { generateToken } from 'src/_utils/token.utils';
+import { UserService } from 'src/user/user.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { SignInAuthDto } from './dto/sign-in-auth.dto';
+import { ArtistService } from 'src/artist/artist.service';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { CreateAuthSocial } from './dto/create-auth-social';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private artistService: ArtistService,
+    private jwtService: JwtService,
+  ) {}
+
+  async create(createAuthDto: CreateAuthDto) {
+    const user = await this.userService.create(createAuthDto);
+
+    return {
+      accessToken: await generateToken(user, this.jwtService),
+      user,
+    };
+  }
+
+  async signIn(signInAuth: SignInAuthDto) {
+    const user = await this.userService.findOneByUsername(signInAuth.username);
+
+    if (user.provider !== 'MY_SYSTEM')
+      throw new BadRequestException(
+        'This user can only login with social media',
+      );
+
+    if (!user) throw new BadRequestException('Bad Credentials');
+
+    const isMatch = await bcrypt.compare(signInAuth.password, user.password);
+    if (!isMatch) throw new BadRequestException('Bad Credentials');
+
+    return {
+      accessToken: await generateToken(user, this.jwtService),
+      user,
+    };
+  }
+
+  async signInSocial(createAuthDto: CreateAuthSocial) {
+    const existingUser = await this.userService.findOneByUsername(
+      createAuthDto.username,
+    );
+
+    if (!existingUser) {
+      const user = await this.userService.create({
+        ...createAuthDto,
+        password: 'test1234',
+      });
+      return {
+        accessToken: await generateToken(user, this.jwtService),
+        user,
+      };
+    }
+
+    return {
+      accessToken: await generateToken(existingUser, this.jwtService),
+      user: existingUser,
+    };
+  }
+
+  async signUpArtist(createArtistDto: CreateArtistDto) {
+    const artist = await this.artistService.create(createArtistDto);
+
+    return {
+      accessToken: await generateToken(artist, this.jwtService),
+    };
+  }
+
+  async signInArtist(createArtistDto: SignInAuthDto) {
+    const user = await this.artistService.findOneByUsername(
+      createArtistDto.username,
+    );
+
+    if (!user) throw new BadRequestException('Bad Credentials');
+
+    const isMatch = await bcrypt.compare(
+      createArtistDto.password,
+      user.password,
+    );
+    if (!isMatch) throw new BadRequestException('Bad Credentials');
+
+    return {
+      accessToken: await generateToken(user, this.jwtService),
+    };
+  }
+}
