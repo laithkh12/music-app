4712848f89c4df160ea0032eca0178da75427323
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-03-17 15:19:25 +0200

    Create main.ts

---
+++ music-backend/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+    prefix: 'api/v',
+  });
+
+  await app.listen(5001);
+}
+bootstrap();
