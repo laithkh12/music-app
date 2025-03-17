c4bba6e7ba23fe2f8abd454844a4644bea539052
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-03-17 15:35:58 +0200

    Create genre.schema.ts

---
+++ music-backend/src/genre/schemas/genre.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type GenreDocument = HydratedDocument<Genre>;
+
+@Schema({ timestamps: true })
+export class Genre {
+  @Prop({ required: true, unique: true })
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  coverImage: string; // generate by client
+
+  @Prop({ default: false })
+  isDelete: boolean;
+}
+
+export const GenreSchema = SchemaFactory.createForClass(Genre);
